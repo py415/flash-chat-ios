4383387bbe404808e4874b24142a46381aab3277
Philip Yu <philipyu1997@gmail.com>
2020-04-05 23:21:53 -0700

    Add animation to app title

--- Flash Chat/Controllers/WelcomeViewController.swift
+++ Flash Chat/Controllers/WelcomeViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    // Outlets
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-       
+        
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        animateText(titleText)
+        
+    }
+    
+    func animateText(_ text: String) {
+        
+        var charIndex = 0.0
+        
+        for letter in text {
+
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            
+            charIndex += 1
+        }
+        
     }
 
 }
